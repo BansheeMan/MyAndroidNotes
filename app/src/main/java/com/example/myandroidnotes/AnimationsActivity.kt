edcc516e20d08f20f502b9332bed14b7525b8098
Banshee <klindyuk98@gmail.com>
2022-06-23 00:00:41 +0300

    Add Explode() к TransactionManager. Реализовали разлетание кнопок

--- app/src/main/java/com/example/myandroidnotes/AnimationsActivity.kt
+++ app/src/main/java/com/example/myandroidnotes/AnimationsActivity.kt
@@ -1,44 +1,70 @@
 package com.example.myandroidnotes
 
+import android.graphics.Rect
 import android.os.Bundle
-import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.*
 import com.example.myandroidnotes.databinding.ActivityAnimationsBinding
 
 class AnimationsActivity : AppCompatActivity() {
 
-    var isOpen: Boolean = false
     private lateinit var binding: ActivityAnimationsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAnimationsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
-            val transitionFade = Fade()
-            transitionFade.duration = 3000
-            val transitionChangeBounds = ChangeBounds()
-            transitionChangeBounds.duration = 1000
-            val transitionSlide = Slide(Gravity.BOTTOM)
-            transitionSlide.duration = 4000
+        binding.recyclerView.adapter = Adapter()
+    }
 
-            val transitionSet = TransitionSet().run {
-                // addTransition(transitionFade)
-                addTransition(transitionChangeBounds)
-                addTransition(transitionSlide)
+    inner class Adapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): mViewHolder {
+            return mViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.activity_animations_recycler_item,
+                    parent,
+                    false
+                ) as View
+            )
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            holder.itemView.setOnClickListener { button ->
+                val epicenter = Rect()
+                button.getGlobalVisibleRect(epicenter)
+                val transitionExplode = Explode()
+                transitionExplode.epicenterCallback = object: Transition.EpicenterCallback() {
+                    override fun onGetEpicenter(transition: Transition): Rect {
+                        return epicenter
+                    }
+                }
+                transitionExplode.duration = 3000
+
+                transitionExplode.excludeTarget(button,true) // исключает цель (нажатую кнопку)
+
+                val transitionFade = Fade().addTarget(button) // выбираем цель для затемнения
+                transitionFade.duration = 5000
+                val transitionSet = TransitionSet().apply {
+                    addTransition(transitionExplode)
+                    addTransition(transitionFade)
+                }
+
+                TransitionManager.beginDelayedTransition(binding.recyclerView, transitionSet)
+                binding.recyclerView.adapter = null
             }
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer, transitionSet)
+        }
 
-            isOpen = !isOpen
-            binding.text.visibility = if (isOpen) {
-                View.VISIBLE
-            } else {
-                View.GONE
-            }
-
+        override fun getItemCount(): Int {
+            return 28
         }
 
     }
+
+    inner class mViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
 }
