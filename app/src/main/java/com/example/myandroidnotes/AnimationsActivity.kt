91fd3673c10882c13e8c695685a14cc86d0fcab2
Banshee <klindyuk98@gmail.com>
2022-06-23 03:37:31 +0300

    Add Роботал с ObjectAnimator и View.animate(), по клику затменялся фон и выплывало меню

--- app/src/main/java/com/example/myandroidnotes/AnimationsActivity.kt
+++ app/src/main/java/com/example/myandroidnotes/AnimationsActivity.kt
@@ -1,15 +1,22 @@
 package com.example.myandroidnotes
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
+import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.transition.*
 import com.example.myandroidnotes.databinding.ActivityAnimationsBinding
 
 class AnimationsActivity : AppCompatActivity() {
 
+    private val duration = 1000L
     private lateinit var binding: ActivityAnimationsBinding
     var isOpen: Boolean = false
 
@@ -19,28 +26,70 @@ class AnimationsActivity : AppCompatActivity() {
         binding = ActivityAnimationsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
+        binding.fab.setOnClickListener {
             isOpen = !isOpen
             ////////////////////////////////////////////////////////////////
-            val trCB = ChangeBounds()
-            val path = ArcMotion()
-            path.maximumAngle = 90.0f
-            trCB.duration = 3000
-            trCB.setPathMotion(path)
-            val trSet = TransitionSet().apply {
-               addTransition(trCB)
-            }
+            if (isOpen) {
+                ObjectAnimator.ofFloat(binding.plusImageview, View.ROTATION, 0f, 135f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, 0f, -135f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, 0f, -250f)
+                    .setDuration(duration).start()
 
-            TransitionManager.beginDelayedTransition(binding.root, trSet)
-            //////////////////////////////////////////////////////////////
-            val params = (binding.button.layoutParams as FrameLayout.LayoutParams)
-            params.gravity = if (isOpen) {
-                Gravity.BOTTOM or Gravity.END
+                binding.optionTwoContainer.animate().alpha(1f).setDuration(duration).setListener(object :AnimatorListenerAdapter(){
+                    override fun onAnimationEnd(animation: Animator?) {
+                        super.onAnimationEnd(animation)
+                        binding.optionTwoContainer.isClickable = true
+                    }
+                })
+
+                binding.optionOneContainer.animate().alpha(1f).setDuration(duration).setListener(object :AnimatorListenerAdapter(){
+                    override fun onAnimationEnd(animation: Animator?) {
+                        super.onAnimationEnd(animation)
+                       binding.optionOneContainer.isClickable = true
+                        Log.d("@@@", "${binding.optionOneContainer.isClickable}")
+
+                    }
+                })
+
+                binding.transparentBackground.animate().alpha(0.6f).duration = duration
+
             } else {
-                Gravity.TOP or Gravity.START
+                ObjectAnimator.ofFloat(binding.plusImageview, View.ROTATION, 135f, 0f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, -135f, 0f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, -250f, 0f)
+                    .setDuration(duration).start()
+
+                binding.optionTwoContainer.animate().alpha(0f).setDuration(duration).setListener(object :AnimatorListenerAdapter(){
+                    override fun onAnimationEnd(animation: Animator?) {
+                        super.onAnimationEnd(animation)
+                        binding.optionTwoContainer.isClickable = false
+                    }
+                })
+
+                binding.optionOneContainer.animate().alpha(0f).setDuration(duration).setListener(object :AnimatorListenerAdapter(){
+                    override fun onAnimationEnd(animation: Animator?) {
+                        super.onAnimationEnd(animation)
+                        binding.optionOneContainer.isClickable = false
+                        Log.d("@@@", "${binding.optionOneContainer.isClickable}")
+
+                    }
+                })
+
+                binding.transparentBackground.animate().alpha(0.0f).duration = duration
             }
-            binding.button.layoutParams = params
         }
+
+
+        binding.optionOneContainer.setOnClickListener { //когда вешаем слушатель isClickable = true
+            Toast.makeText(this, "optionOneContainer", Toast.LENGTH_SHORT).show()
+
+        }
+        binding.optionOneContainer.isClickable = false
+        Log.d("@@@", "${binding.optionOneContainer.isClickable}")
 
     }
 
